d922ed93b4ec7ca7de9b40001583054b61119583
PedroDressler <pedro2003dressler@gmail.com>
2024-04-06 18:43:29 -0300

    push to main

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
 import { usersRoutes } from './routes/user'
 import { mealsRoutes } from './routes/meals'
+import cookie from '@fastify/cookie'
 
 export const app = fastify()
+
+// Plugins
+app.register(cookie)
 
 // Routes
 app.register(usersRoutes, { prefix: '/users' })
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,73 @@
 import { FastifyInstance } from 'fastify'
+import { database } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
+import { checkIfSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', async (request, reply) => {
-    return reply.status(200).send()
+  const userBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  })
+
+  app.get(
+    '/',
+    { preHandler: [checkIfSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+
+      const user = await database('users')
+        .where('session_id', sessionId)
+        .select()
+
+      return reply.status(200).send({ user })
+    },
+  )
+
+  app.post('/', async (request, reply) => {
+    const { name, email } = userBodySchema.parse(request.body)
+
+    const userByEmail = await database('users').where({ email }).first()
+
+    if (userByEmail) {
+      return reply.status(400).send({ message: 'User already exists!' })
+    }
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('session_id', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await database('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+
+  app.post('/login', async (request, reply) => {
+    const { name, email } = userBodySchema.parse(request.body)
+
+    const userExists = await database('users').where({ name, email }).first()
+
+    if (!userExists) {
+      return reply.status(404).send({ message: 'User does not exist!' })
+    }
+
+    const sessionId = randomUUID()
+
+    reply.setCookie('session_id', sessionId, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    })
   })
 }
