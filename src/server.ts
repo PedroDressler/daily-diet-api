6f201018d93cf802dc9c86e47982c378dd8c8826
PedroDressler <pedro2003dressler@gmail.com>
2024-04-09 19:13:29 -0300

    try catch on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT, host: 'localhost' }).then(() => {
-  console.log('HTTP Server Running')
-})
+try {
+  app.listen({ port: env.PORT, host: 'localhost' }).then(() => {
+    console.log('HTTP Server Running')
+  })
+} catch (err) {
+  app.log.error(err)
+
+  process.exit(1)
+}
