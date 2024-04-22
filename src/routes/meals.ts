c333746911f6d51165c5b27b4defb2936f2e204a
PedroDressler <pedro2003dressler@gmail.com>
2024-04-22 06:31:58 -0300

    added PUT /meals route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -7,83 +7,119 @@ import { randomUUID } from 'crypto'
 const options = { prefix: '/meals' }
 
 async function handler(app: FastifyInstance) {
-  const createMealBodySchema = z.object({
-    name: z.string(),
-    description: z.string().default(''),
-    date: z.coerce.date(),
-    isOnDiet: z.boolean().default(false),
-    rating: z.number().optional(),
+  app.addHook('preHandler', async (request, reply) => {
+    await checkIfSessionIdExists(request, reply)
   })
 
   // GET /meals
-  app.get(
-    '/',
-    { preHandler: [checkIfSessionIdExists] },
-    async (request, reply) => {
-      const id = request.user?.id
+  app.get('/', async (request, reply) => {
+    const id = request.user?.id
 
-      const userMeals = await database('meals')
-        .where('user_id', id)
-        .select(
-          'meals.id as meal_id',
-          'meals.name',
-          'meals.description',
-          'meals.date as meal_date',
-          'meals.is_on_diet',
-          'meals.rating',
-          'meals.created_at',
-          'meals.updated_at',
-        )
+    const userMeals = await database('meals')
+      .where('user_id', id)
+      .select(
+        'meals.id as meal_id',
+        'meals.name',
+        'meals.description',
+        'meals.date as meal_date',
+        'meals.is_on_diet',
+        'meals.rating',
+        'meals.created_at',
+        'meals.updated_at',
+      )
 
-      return reply.status(200).send({ user: request.user?.name, userMeals })
-    },
-  )
+    return reply.status(200).send({ user: request.user?.name, userMeals })
+  })
 
   // POST /meals
-  app.post(
-    '/',
-    { preHandler: [checkIfSessionIdExists] },
-    async (request, reply) => {
-      const { name, date, description, isOnDiet, rating } =
-        createMealBodySchema.parse(request.body)
+  app.post('/', async (request, reply) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string().default(''),
+      date: z.coerce.date(),
+      isOnDiet: z.boolean().default(false),
+      rating: z.number().optional(),
+    })
 
-      const mealPostResponse = await database('meals').insert({
-        id: randomUUID(),
-        name,
-        description,
-        is_on_diet: isOnDiet,
-        date: date.getTime(),
-        user_id: request.user?.id,
-        rating,
+    const { name, date, description, isOnDiet, rating } =
+      createMealBodySchema.parse(request.body)
+
+    const mealPostResponse = await database('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      is_on_diet: isOnDiet,
+      date: date.getTime(),
+      user_id: request.user?.id,
+      rating,
+    })
+
+    return reply
+      .status(201)
+      .send({ message: 'Meal created', responseCode: mealPostResponse })
+  })
+
+  // DELETE /meals/:mealId
+  app.delete('/:mealId', async (request, reply) => {
+    const paramsSchema = z.object({
+      mealId: z.string().uuid(),
+    })
+
+    const { mealId } = paramsSchema.parse(request.params)
+
+    const meal = await database('meals').where({ id: mealId })
+
+    if (!meal) {
+      return reply.status(404).send({ message: 'Meal not found!' })
+    }
+
+    await database('meals').where({ id: mealId }).del()
+
+    return reply.status(204).send({ message: 'Meal deleted successfully' })
+  })
+
+  // PUT /meals/:mealId
+  app.put('/:mealId', async (request, reply) => {
+    const updateMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      isOnDiet: z.boolean(),
+      rating: z.number(),
+    })
+
+    const { name, description, date, isOnDiet, rating } =
+      updateMealBodySchema.parse(request.body)
+
+    const paramsSchema = z.object({
+      mealId: z.string().uuid(),
+    })
+
+    const { mealId } = paramsSchema.parse(request.params)
+
+    const userId = request.user?.id
+
+    const meal = await database('meals')
+      .where({
+        user_id: userId,
+        id: mealId,
       })
+      .first()
 
-      return reply
-        .status(201)
-        .send({ message: 'Meal created', responseCode: mealPostResponse })
-    },
-  )
+    if (!meal) {
+      return reply.status(404).send({ message: 'Meal not found' })
+    }
 
-  app.delete(
-    '/:mealId',
-    { preHandler: [checkIfSessionIdExists] },
-    async (request, reply) => {
-      const paramsSchema = z.object({
-        mealId: z.string().uuid(),
-      })
+    await database('meals').where({ id: mealId, user_id: userId }).update({
+      name,
+      description,
+      date: date.getTime(),
+      is_on_diet: isOnDiet,
+      rating,
+    })
 
-      const { mealId } = paramsSchema.parse(request.params)
-
-      const meal = await database('meals').where({ id: mealId })
-
-      if (!meal) {
-        return reply.status(404).send({ message: 'Meal not found!' })
-      }
-
-      await database('meals').where({ id: mealId }).del()
-
-      return reply.status(204).send({ message: 'Meal deleted successfully' })
-    },
-  )
+    return reply.status(204).send()
+  })
 }
 
 export default { handler, options }
